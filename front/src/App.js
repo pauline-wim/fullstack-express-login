df9904a5235d53d6736eeb2e95557d003335dda7
pauline-wim <linette.wim@hotmail.com>
2022-03-28 15:30:28 +0200

    Setup frontend

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Admin from "./pages/Admin";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{ color: "red" }}>Authentification</h1>
+      <Signup />
+      <Login />
+      <Admin />
     </div>
   );
 }
