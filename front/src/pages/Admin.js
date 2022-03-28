0da6aeb12541f4643b7f222365a9ab27111e76fa
pauline-wim <linette.wim@hotmail.com>
2022-03-28 17:09:46 +0200

    Show all users full name & date of birth

--- front/src/pages/Admin.js
+++ front/src/pages/Admin.js
@@ -1,7 +1,36 @@
+import { useContext } from "react";
+// Context
+import { UsersContext } from "../App";
+
 export default function Admin() {
+  const users = useContext(UsersContext);
+
+  function getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    return age;
+  }
+
   return (
     <>
       <h1>Admin</h1>
+      <ul>
+        {users.users.map((user, i) => {
+          return (
+            <li key={i}>
+              {user.firstName}{" "}
+              <span style={{ fontWeight: 700 }}>{user.lastName}</span>
+              <br />
+              Age: {getAge(user.dateOfBirth)}
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 }
