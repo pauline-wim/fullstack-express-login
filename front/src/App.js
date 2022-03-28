a3b5c76a737554432884c2675d33ca56266e7f86
pauline-wim <linette.wim@hotmail.com>
2022-03-28 16:37:36 +0200

    Create form in Signup page => Save data from user

--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,36 @@
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Admin from "./pages/Admin";
+import { createContext, useState, useEffect } from "react";
+
+export const UsersContext = createContext();
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const usersContext = {
+    users,
+    setUsers,
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:8000/")
+      .then((res) => res.json(res))
+      .then((res) => {
+        // console.log(res);
+        setUsers(res);
+      });
+  }, []);
+
   return (
-    <div>
-      <h1 style={{ color: "red" }}>Authentification</h1>
-      <Signup />
-      <Login />
-      <Admin />
-    </div>
+    <UsersContext.Provider value={usersContext}>
+      <div>
+        <h1 style={{ color: "red" }}>Authentification</h1>
+        <Signup />
+        <Login />
+        <Admin />
+      </div>
+    </UsersContext.Provider>
   );
 }
 
--- front/src/pages/Signup.js
+++ front/src/pages/Signup.js
@@ -1,7 +1,97 @@
+import { useForm } from "react-hook-form";
+import { useContext } from "react";
+// Context
+import { UsersContext } from "../App";
+
 export default function Signup() {
+  const users = useContext(UsersContext);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    users.setUsers((prev) => [
+      ...prev,
+      {
+        id: users.users.length + 1,
+        email: data.emailEntry,
+        password: data.pwdEntry,
+        firstName: data.firstNameEntry,
+        lastName: data.lastNameEntry,
+        dateOfBirth: data.dateEntry,
+      },
+    ]);
+    fetch("http://localhost:8000/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: data.emailEntry,
+        password: data.pwdEntry,
+        firstName: data.firstNameEntry,
+        lastName: data.lastNameEntry,
+        dateOfBirth: data.dateEntry,
+      }),
+    });
+  };
+
   return (
     <>
       <h1>Signup</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>
+          Email:
+          <input type="email" {...register("emailEntry", { required: true })} />
+          {errors.emailEntry && <span>Please enter a valid email</span>}
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            {...register("pwdEntry", { required: true })}
+          />
+          {errors.pwdEntry && <span>Please enter a valid password</span>}
+        </label>
+        <label>
+          Confirm password:
+          <input
+            type="password"
+            {...register("confirmPwdEntry", { required: true })}
+          />
+          {errors.confirmPwdEntry && <span>This password is incorrect</span>}
+        </label>
+        <br />
+        <label>
+          First Name:
+          <input
+            type="text"
+            {...register("firstNameEntry", { required: true })}
+          />
+          {errors.firstNameEntry && <span>Please enter your first name</span>}
+        </label>
+        <br />
+        <label>
+          Last Name:
+          <input
+            type="text"
+            {...register("lastNameEntry", { required: true })}
+          />
+          {errors.lastNameEntry && <span>A last name is required</span>}
+        </label>
+        <br />
+        <label>
+          Date of birth:
+          <input type="date" {...register("dateEntry", { required: true })} />
+          {errors.dateEntry && <span>Please enter a date of birth</span>}
+        </label>
+        <br />
+        <input type="submit" value="Save" />
+      </form>
     </>
   );
 }
